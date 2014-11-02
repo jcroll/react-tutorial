a120f32e415392c4ab85c49f5e811ae50e40fd06
Jarrett Croll <jarrett.croll@gmail.com>
2014-11-02 18:02:25 -0500

    add comments array to php session

--- src/controllers.php
+++ src/controllers.php
@@ -9,19 +9,24 @@ use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 //Request::setTrustedProxies(array('127.0.0.1'));
 
 $app->get('/', function () use ($app) {
+
     return $app['twig']->render('index.html', array());
 })
 ->bind('homepage')
 ;
 
 $api = $app['controllers_factory'];
-$api->get('/comments', function () {
-    $comments = [
-        ['author' => 'Pete Hunt',    'text' => 'This is one comment'],
-        ['author' => 'Jordan Walke', 'text' => 'This is *another* comment']
-    ];
+$api->get('/comments', function () use($app) {
+    $session = $app['session'];
+    if (!$session->has('comments')) {
+        $session->set('comments', [
+            ['author' => 'Pete Hunt',    'text' => 'This is one comment'],
+            ['author' => 'Jordan Walke', 'text' => 'This is *another* comment']
+        ]);
+    }
 
-    return new JsonResponse($comments);
+
+    return new JsonResponse($session->get('comments'));
 })
 ->bind('api_comments')
 ;
