e0d466f4e5ddb76e51a9a90e6a3664d37c6c960c
Jarrett Croll <jarrett.croll@gmail.com>
2014-11-02 16:35:11 -0500

    hook in json data

--- web/js/comments-box.jsx
+++ web/js/comments-box.jsx
@@ -1,9 +1,13 @@
+var data = [
+    {author: "Pete Hunt", text: "This is one comment"},
+    {author: "Jordan Walke", text: "This is *another* comment"}
+];
 var CommentBox = React.createClass({
     render: function() {
         return (
             <div className="commentBox">
                 <h1>Comments</h1>
-                <CommentList />
+                <CommentList data={this.props.data} />
                 <CommentForm />
             </div>
         );
@@ -11,14 +15,34 @@ var CommentBox = React.createClass({
 });
 var CommentList = React.createClass({
     render: function() {
+        var commentNodes = this.props.data.map(function (comment) {
+            return (
+                <Comment author={comment.author}>
+                    {comment.text}
+                </Comment>
+            );
+        });
         return (
             <div className="commentList">
-            Hello, world! I am a CommentList.
+                {commentNodes}
             </div>
         );
     }
 });
-
+var converter = new Showdown.converter();
+var Comment = React.createClass({
+    render: function() {
+        var rawMarkup = converter.makeHtml(this.props.children.toString());
+        return (
+            <div className="comment">
+                <h2 className="commentAuthor">
+                    {this.props.author}
+                </h2>
+                <span dangerouslySetInnerHTML={{__html: rawMarkup}} />
+            </div>
+        );
+    }
+});
 var CommentForm = React.createClass({
     render: function() {
         return (
@@ -29,6 +53,6 @@ var CommentForm = React.createClass({
     }
 });
 React.render(
-    <CommentBox />,
+    <CommentBox data={data} />,
     document.getElementById('content')
 );
